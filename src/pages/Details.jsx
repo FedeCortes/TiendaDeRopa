210870cdef7dc3940fa5000c5702e605426202e8
Fede <fedecortes01@gmail.com>
2023-08-11 14:15:32 -0300

    Estilos del styles, y corazon.

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,49 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 const Details = () => {
   const [productos, setProductos] = useState([]);
-
-  const getProductos = async () => {
-    const resp = await axios.get('https://fakestoreapi.com/products');
-    setProductos(resp.data);
-  };
-
-  useEffect(() => {
-    getProductos();
-  }, []);
-
   const { id } = useParams();
   const productId = parseInt(id, 10);
   const producto = productos.find((p) => p.id === productId);
 
+  useEffect(() => {
+    const getProductos = async () => {
+      try {
+        const resp = await axios.get('https://fakestoreapi.com/products');
+        setProductos(resp.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    getProductos();
+  }, []);
+
   return (
-    <div>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
       {producto && (
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <div style={{ marginRight: '20px' }}>
+        <div style={{ display: 'flex', alignItems: 'center', maxWidth: '800px' }}>
+          <div style={{ marginRight: '5px' }}>
             <img
               src={producto.image}
               alt={producto.title}
               style={{ width: '500px', height: '500px' }}
             />
           </div>
-          <div >
-            <div style={{marginBottom:"25px"}}>
-            <Typography variant="h3">{producto.title}</Typography>
-            <Typography>${producto.price}</Typography>
+          <div>
+            <div style={{ marginBottom: '25px' }}>
+              <Typography variant="h4">{producto.title} <FavoriteIcon color="error" style={{ marginLeft: '10px' }} /></Typography>
+              <Typography>${producto.price}</Typography>
             </div>
-            
             <Button variant="contained" style={{ backgroundColor: 'black', color: 'white' }}>
               Agregar al carrito
             </Button>
-            <div style={{marginTop:"25px",marginLeft:"80px",marginRight:"80px"}}>
-            <Typography variant="p">{producto.description}</Typography>
+            
+            <div style={{ marginTop: '25px' }}>
+              <Typography>{producto.description}</Typography>
             </div>
+            
           </div>
         </div>
       )}
